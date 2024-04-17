2a7e1f03ba7158b27a04767c8aa0945ac5e08409
Antonio López <tonidiez17@gmail.com>
2024-04-17 10:51:04 +0200

    Subida login, ERROR --Antonio

--- src/Paginas/Login/login-style.js
+++ src/Paginas/Login/login-style.js
@@ -1,6 +1,17 @@
 import styled from "styled-components";
 
-
 export const Div = styled.div`
   margin-top: 10em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color:red;
+`;
+
+export const Input = styled.input`
+  width: 15vw;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
 `;
